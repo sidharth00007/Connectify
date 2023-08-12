d1290a509df5ff0e8b325488f5beec5952739ee6
sidharth00007 <championsid07@gmail.com>
2023-08-12 16:15:10 +0530

    added render url

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -23,7 +23,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
 
   const patchFriend = async () => {
     const response = await fetch(
-      `http://localhost:3001/users/${_id}/${friendId}`,
+      `https://connectify-server-xnu0.onrender.com/users/${_id}/${friendId}`,
       {
         method: "PATCH",
         headers: {
--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -8,7 +8,7 @@ const UserImage = ({ image, size = "60px" }) => {
         width={size}
         height={size}
         alt="user"
-        src={`http://localhost:3001/assets/${image}`}
+        src={`https://connectify-server-xnu0.onrender.com/assets/${image}`}
       />
     </Box>
   );
